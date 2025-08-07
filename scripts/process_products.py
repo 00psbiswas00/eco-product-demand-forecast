b3b40a97659cbff2536fad60565e839015e387a3
00psbiswas00 <60921014+00psbiswas00@users.noreply.github.com>
2025-08-08 00:46:27 +0530

    remove logs

--- scripts/process_products.py
+++ scripts/process_products.py
@@ -6,22 +6,11 @@ raw_table: list[str]=['openfoodfacts_raw', 'openbeautyfacts_raw']
 def clean_prodct_data(table:str)->pd.DataFrame:
     con= sqlite3.connect('data/raw/products.db')
     df=pd.read_sql(f'SELECT * FROM {table};', con)
-    print(df.head(2))
-    print('Null before cleanign\n')
-    print(df.isna().sum()/df.shape[0]*100)
     df=df.dropna(subset=['product_name','url'])
     for col in ['brand','categories','labels','packaging','ecoscore_grade','nutriscore_grade']:
         if col in df.columns:
             df[col] = df[col].fillna('unknown')
-    print('Null after cleanign\n')
-    print(df.isna().sum()/df.shape[0]*100)
-
-    print('duplicate after drop null\n')
-    print(df.duplicated(subset=['url']).sum())
     df=df.drop_duplicates(subset=['url'])
-    print('duplicate after drop null\n')
-    print(df.duplicated(subset=['url']).sum())
-
     return df
 
 
